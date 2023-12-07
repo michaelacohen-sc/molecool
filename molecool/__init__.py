7025c726eee958f0ad6b302e9f1118a7a9cadb36
Steven <steven.ayoub.362@my.csun.edu>
2023-12-06 19:33:34 -0800

    ending point for python testing

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -4,5 +4,6 @@
 from .functions import canvas
 from .io import open_pdb, open_xyz, write_xyz
 from .measure import calculate_angle, calculate_distance
-from .molecule import build_bond_list
+from .molecule import (build_bond_list, calculate_center_of_mass,
+                       calculate_molecular_mass)
 from .visualize import bond_histogram, draw_molecule
